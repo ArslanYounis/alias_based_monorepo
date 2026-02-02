f077022af52a8cf7fa74304a1db97d1908171617
Arslan Younis <arslancs15@gmail.com>
2026-02-02 15:36:33 +0500

    mobile converted to dev client

--- mobile/index.ts
+++ mobile/index.ts
@@ -4,6 +4,6 @@ import "./global.css";
 import App from './src/app/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
+// It also ensures that whether you load the app in a dev client or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
--- mobile/src/app/App.tsx
+++ mobile/src/app/App.tsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { View, StyleSheet, Text } from "react-native";
-import { CustomDrawer } from "~/lib-index";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { CustomDrawer, DateInput, TextInput } from "~/lib-index";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
+  const [open, setOpen] = useState(false);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={styles.container}>
         <StatusBar style="auto" />
-        <CustomDrawer open>
-          <View>
-            <Text>Layer 1</Text>
-          </View>
-        </CustomDrawer>
+        <TextInput label="Name" placeholder="Enter your name" />
+        <DateInput
+          label="Date of Birth"
+          placeholder="Select your date of birth"
+        />
       </View>
     </GestureHandlerRootView>
   );
