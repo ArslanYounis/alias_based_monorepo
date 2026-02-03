bf2bf2d13a7607d3432efcde8a58e3bbb9413671
Arslan Younis <arslancs15@gmail.com>
2026-02-03 16:26:57 +0500

    dependencies fixed

--- web/src/utils/expose.components.ts
+++ web/src/utils/expose.components.ts
@@ -1,10 +1,11 @@
 import ButtonsConfigs from "@/ui/Buttons/buttons.config";
 import DummyConfigs from "@/ui/configs/Dummy.config";
+import PlotSearchConfigs from "@/ui/configs/PlotSearch.config";
 
 /**
  * Array of all component configurations to be exposed to service-builder
  * Add new component configs here to make them available in the service builder
  */
-const exposeComponents = [ButtonsConfigs, DummyConfigs];
+const exposeComponents = [ButtonsConfigs, DummyConfigs, PlotSearchConfigs];
 
 export default exposeComponents;
